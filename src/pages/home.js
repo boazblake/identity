8c46c000b732cf27e27e1a07c1b43d8c055c2963
Boaz Blake <boazblake@gmail.com>
2024-06-09 07:35:39 -0500

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,7 +17,7 @@ const getRightStyle = ({ settings: { profile } }) => {
 const getLeftStyle = ({ settings: { profile } }) => {
   switch (profile) {
     case 'phone':
-      return { height: "120vh", marginBottom: '10px' }
+      return { height: "120vh", paddingBottom: '10px' }
     case "tablet":
       return { height: "100%", justifyContent: 'center' }
   }
