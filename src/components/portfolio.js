7a8d1797949c0b4357731455085c5b55ba042440
Boaz Blake <boazblake@gmail.com>
2025-04-20 10:53:48 -0500

    updating ewpos

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,39 +1,37 @@
-import m from 'mithril'
+import m from "mithril";
 
 const hasRepos = () => {
-  let age = parseInt(localStorage.getItem("repos-date"))
-  let now = Date.now()
+  let age = parseInt(localStorage.getItem("repos-date"));
+  let now = Date.now();
 
   return (now - age) / 1000 >= 3600
     ? (localStorage.clear("repos"), null)
-    : localStorage.getItem("repos")
-}
+    : localStorage.getItem("repos");
+};
 
 const fetchRepos = (mdl) => {
-  mdl.portfolio.reposList = JSON.parse(hasRepos())
-  return mdl.portfolio.reposList
-}
+  mdl.portfolio.reposList = JSON.parse(hasRepos());
+  return mdl.portfolio.reposList;
+};
 const saveRepos = (repos) => {
-  localStorage.setItem("repos-date", JSON.stringify(Date.now()))
-  localStorage.setItem("repos", JSON.stringify(repos))
-  return repos
-}
+  localStorage.setItem("repos-date", JSON.stringify(Date.now()));
+  localStorage.setItem("repos", JSON.stringify(repos));
+  return repos;
+};
 
-const hasRepo = (name) => localStorage.getItem(name)
-const fetchRepo = (name) => JSON.parse(hasRepo(name))
+const hasRepo = (name) => localStorage.getItem(name);
+const fetchRepo = (name) => JSON.parse(hasRepo(name));
 const saveRepo = (name) => (repo) => {
-  localStorage.setItem(name, JSON.stringify(repo))
-  return repo
-}
+  localStorage.setItem(name, JSON.stringify(repo));
+  return repo;
+};
 
 const handler = (entry) =>
   entry.forEach(
-    (change) => (change.target.style.opacity = change.isIntersecting ? 1 : 0)
-  )
+    (change) => (change.target.style.opacity = change.isIntersecting ? 1 : 0),
+  );
 
-const opacityObs = new IntersectionObserver(handler)
-
-
+const opacityObs = new IntersectionObserver(handler);
 
 const getRepos = (mdl) =>
   m
@@ -43,7 +41,7 @@ const getRepos = (mdl) =>
         Accept: "application/vnd.github.v3+json",
       },
     })
-    .then(saveRepos)
+    .then(saveRepos);
 
 const getRepo = (state) =>
   hasRepo(state.name)
@@ -52,32 +50,34 @@ const getRepo = (state) =>
       .request({
         url: `https://api.github.com/repos/boazblake/${state.name}`,
       })
-      .then(saveRepo(state.name))
+      .then(saveRepo(state.name));
 
 const Repo = () => {
   const state = {
     name: "",
     status: "loading",
-  }
+  };
   return {
-    oninit: ({ attrs: { mdl, url } }) => {
-      state.name = url.split("/")[3]
+    oninit: ({ attrs: { mdl, name, url } }) => {
+      state.name = name;
+      console.log("??", state.name);
       getRepo(state).then(
-        ({ description }) => {
-          state.errors = null
-          state.info = description && description.split("~")[0]
-          state.src = description && description.split("~")[1]
-          state.status = "loaded"
-          mdl.portfolio.repos[state.name] = { description }
-          hasRepo(state.name) && m.redraw()
+        ({ description, homepage }) => {
+          state.errors = null;
+          state.info = description && description.split("~")[0];
+          state.src = description && description.split("~")[1];
+          state.status = "loaded";
+          mdl.portfolio.repos[name] = { name, description, homepage };
+          hasRepo(state.name) && m.redraw();
         },
         (errors) => {
-          state.status = "failed"
-          state.errors = errors
-        }
-      )
+          state.status = "failed";
+          state.errors = errors;
+        },
+      );
     },
-    view: ({ attrs: { idx } }) => {
+    view: ({ attrs: { url, name, idx } }) => {
+      console.log(url, name);
       return (
         state.status == "loading" && "Repo Loading...",
         state.status == "failed" && "Error",
@@ -85,35 +85,40 @@ const Repo = () => {
         m(
           "a.w3-col s12 m6 l4",
           {
-            pointerEvents: 'auto',
+            pointerEvents: "auto",
             zIndex: 999,
             tabIndex: idx + 1,
-            href: `https://boazblake.github.io/${state.name}`,
+            href: url, //,`https://boazblake.github.io/${state.name}`,
             target: "_blank",
             rel: "noopener noreferrer",
-            oncreate: ({ dom }) => { state.status == "loaded" && opacityObs.observe(dom) },
+            oncreate: ({ dom }) => {
+              state.status == "loaded" && opacityObs.observe(dom);
+            },
             style: { opacity: 1 },
           },
           m(
             ".w3-cell.w3-padding-small",
-            m('h2', {
-              onclick: () => window.location.href = `https://boazblake.github.io/${state.name}`,
-
-            }, state.name),
+            m(
+              "h2",
+              {
+                onclick: () => (window.location.href = url),
+              },
+              state.name,
+            ),
             m("img", { style: { maxWidth: "80%" }, src: state.src }),
             m(".info", state.info),
-          )
+          ),
         )
-      )
+      );
     },
-  }
-}
+  };
+};
 
 const Portfolio = () => {
   const state = {
     status: "loading",
     errors: {},
-  }
+  };
 
   return {
     oninit: ({ attrs: { mdl } }) =>
@@ -121,49 +126,63 @@ const Portfolio = () => {
         .then((repos) =>
           repos
             .filter((repo) => {
+              console.log(repo);
               return (
                 repo.homepage &&
                 repo.homepage.includes("boazblake") &&
                 repo.description &&
                 repo.description.split("~")[1]
-              )
+              );
             })
-            .map((repo) => repo.homepage)
+            .map((repo) => ({ url: repo.homepage, name: repo.name })),
         )
         .then(
           (repos) => {
-            mdl.portfolio.reposList = repos
-            state.status = "loaded"
-            hasRepos() && m.redraw()
+            mdl.portfolio.reposList = repos;
+            state.status = "loaded";
+            hasRepos() && m.redraw();
           },
           (errors) => {
-            state.status = "failed"
-            state.errors = errors
-          }
+            state.status = "failed";
+            state.errors = errors;
+          },
         ),
     view: ({ attrs: { mdl } }) =>
       m(
         ".w3-container",
-        m('h1', 'Projects'),
-        m('h2.w3-center', 'Click through to visit these projects'),
+        m("h1", "Projects"),
+        m("h2.w3-center", "Click through to visit these projects"),
         state.status == "failed" && "Error fetching Repos ...",
-        state.status == "loading" && m('.w3-panel', "Loading Repos ..."),
+        state.status == "loading" && m(".w3-panel", "Loading Repos ..."),
         state.status == "loaded" &&
-        m('.w3-row.w3-grid.overflow',
-          m('a.w3-col s12 m6 l4', {
-            href: `https://bonhamacres.org`,
-            target: "_blank",
-            tabIndex: 0,
-            onclick: () => window.location.href = `https://bonhamacres.org`,
-          },
-            m('h2', 'bonhamacres.org'),
+        m(
+          ".w3-row.w3-grid.overflow",
+          m(
+            "a.w3-col s12 m6 l4",
+            {
+              href: `https://bonhamacres.org`,
+              target: "_blank",
+              tabIndex: 0,
+              onclick: () =>
+                (window.location.href = `https://bonhamacres.org`),
+            },
+            m("h2", "bonhamacres.org"),
 
-            m('img', { style: { maxWidth: '80%', height: 'auto', zIndex: 999 }, src: 'images/baca.webp' }), m('p', 'Neighborhood Civic Association website that I am webmaster of. Created using mithriljs and expressjs')),
-          mdl.portfolio.reposList.map((url, idx) => m(Repo, { url, mdl, idx }))
-        )
+            m("img", {
+              style: { maxWidth: "80%", height: "auto", zIndex: 999 },
+              src: "images/baca.webp",
+            }),
+            m(
+              "p",
+              "Neighborhood Civic Association website that I am webmaster of. Created using mithriljs and expressjs",
+            ),
+          ),
+          mdl.portfolio.reposList.map(({ url, name }, idx) =>
+            m(Repo, { url, name, mdl, idx }),
+          ),
+        ),
       ),
-  }
-}
+  };
+};
 
-
-export { Portfolio }
+export { Portfolio };
